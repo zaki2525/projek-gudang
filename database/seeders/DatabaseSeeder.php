03773b4a3f30c129d1c2d2f2b3a90684a441b9e5
tgrsnts <tegarsantoso72@gmail.com>
2023-01-05 10:48:06 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create([           
+            'name' => "admin",
+            'email' => "admin@cork.com",
+            'password' => bcrypt('password'),
+        ]);
     }
 }
