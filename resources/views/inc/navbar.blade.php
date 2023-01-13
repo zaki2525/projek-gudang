a9914c641c0580bb6ff272ca80284ab6b1b410e8
tgrsnts <tegarsantoso72@gmail.com>
2023-01-13 15:03:44 +0700

    :

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -7,7 +7,9 @@
             <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></a>
 
             <div class="nav-logo align-self-center">
-            <a class="navbar-brand" href="/sales"><img alt="logo" src="{{asset('storage/img/90x90.jpg')}}"> <span class="navbar-brand-name">MEET</span></a>
+            <a class="navbar-brand" href="/dashboard"><img alt="logo" src="{{asset('storage/img/meet.jpg')}}" style="width:100px"> 
+                {{-- <span class="navbar-brand-name">MEET</span> --}}
+            </a> 
             </div>
 
             @if ($category_name != 'starter_kits')
