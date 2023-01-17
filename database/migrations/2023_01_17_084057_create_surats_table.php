e550840665dd94ab0152081afd781f957d4cf510
tgrsnts <tegarsantoso72@gmail.com>
2023-01-17 08:44:31 +0700

    surats

---
+++ database/migrations/2023_01_17_084057_create_surats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surats', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_project');
+            $table->integer('id_barang');
+            $table->string('delivery');
+            $table->string('kepada');
+            $table->string('no_sj');
+            $table->string('no_mobil');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surats');
+    }
+};
