c794e382719f341fa959ee9c3fbc3c22be67d98b
zaki2525 <mzakis272@gmail.com>
2023-01-23 11:38:41 +0700

    p

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Login | E-Inventory</title>
 
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
     <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
@@ -30,8 +30,8 @@
             <div class="form-form-wrap">
                 <div class="form-container">
                     <div class="form-content">
-
-                        <h1 class="">Sign In</h1>
+                    <img alt="logo" src="{{asset('storage/img/meet.jpg')}}" style="width: 400px;">
+                    <!-- <h1 class="">E-Inventory</h1> -->
                         <p class="">Log in to your account to continue.</p>
                         
                         <form method="POST" action="{{ route('login') }}" class="text-left">
@@ -71,21 +71,6 @@
                                     <div class="field-wrapper">
                                         <button type="submit" class="btn btn-primary" value="">{{ __('Login') }}</button>
                                     </div>
-                                </div>
-
-                                <div class="division">
-                                      <span>OR</span>
-                                </div>
-
-                                <div class="social">
-                                    <a href="javascript:void(0);" class="btn social-fb">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg> 
-                                        <span class="brand-name">Facebook</span>
-                                    </a>
-                                   <a href="javascript:void(0);" class="btn social-github">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
-                                        <span class="brand-name">Github</span>
-                                    </a>
                                 </div>
 
 
