aa6ca9af405d82174dd6d243fb06f843a5107755
zaqqye <mzakis272@gmail.com>
2023-02-11 16:16:31 +0700

    menghilangkan action pada role user di yajra datatable

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -54,6 +54,7 @@
                         </div>
                         <!-- END TAMBAH DATA -->
                     </div>
+                    @if(auth()->user()->role == 'admin')
                     <div class="table-responsive mb-4 mt-4">
                         <table class="table table-hover non-hover data-table" style="width:100%">
                             <thead>
@@ -174,6 +175,127 @@
                             </tbody>
                         </table>
                     </div>
+                    @else
+                    <div class="table-responsive mb-4 mt-4">
+                        <table class="table table-hover non-hover data-tableuser" style="width:100%">
+                            <thead>
+                                <tr align="center">
+                                    <th>No</th>
+                                    <th>Material Name</th>
+                                    <th>Unit</th>
+                                    <th>Stock</th>
+                                </tr>
+                            </thead>
+                            <tbody align="center">
+                                {{-- @foreach ($datas as $item)
+                                    <tr align="center">
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->namaBarang->nama }}</td>
+                                        <td>{{ $item->namaBarang->unit }}</td>
+                                        <td>{{ $item->stock + $item->totalBarang() }}</td>
+                                        <td>
+                                            <a href="/barang/{{ $item->id }}">                                        
+                                                <button type="button" class="btn btn-primary btn-sm">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-eye">
+                                                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                        <circle cx="12" cy="12" r="3"></circle>
+                                                    </svg>
+                                                </button>
+                                            </a>
+                                            @if(auth()->user()->role == 'admin')
+                                            <button type="button" class="btn btn-dark btn-sm" data-toggle="modal"
+                                                data-target="#modalEditData{{ $item->id }}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="feather feather-edit">
+                                                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7">
+                                                    </path>
+                                                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z">
+                                                    </path>
+                                                </svg>
+                                            </button>
+                                            <!-- HAPUS DATA -->
+                                            <form action="/barang/{{ $item->id }}" method="POST" class="d-inline delete">
+                                                @method('delete')
+                                                @csrf
+                                                <button type="submit" class="btn btn-danger btn-sm warning cancel"
+                                                    onclick="return false">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-trash-2">
+                                                        <polyline points="3 6 5 6 21 6"></polyline>
+                                                        <path
+                                                            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                                        </path>
+                                                        <line x1="10" y1="11" x2="10" y2="17">
+                                                        </line>
+                                                        <line x1="14" y1="11" x2="14" y2="17">
+                                                        </line>
+                                                    </svg>
+                                                </button>
+                                            </form>
+                                            <!-- END HAPUS DATA -->
+                                            @endif
+                                        </td>
+                                    </tr>
+
+                                    <!-- MODAL EDIT DATA -->
+                                    <div class="modal fade" id="modalEditData{{ $item->id }}" tabindex="-1"
+                                        aria-labelledby="modalEditBarang" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="title">Edit Barang
+                                                    </h5>
+                                                    <button type="button" class="close" data-dismiss="modal"
+                                                        aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form method="POST" action="barang/{{ $item->id }}"
+                                                        id="addUser" class="update">
+                                                        @csrf
+                                                        @method('put')
+                                                        <div class="form-floating mb-3">
+                                                            <label for="floatingInput2">Material Name</label>
+                                                            <input
+                                                                value="{{ old('nama', $item->namaBarang->nama) }}"
+                                                                required name="nama" type="text" required
+                                                                class="form-control" id="nama">
+                                                        </div>
+                                                        <div class="form-floating mb-3">
+                                                            <label for="floatingInput4">Unit</label>
+                                                            <input value="{{ old('unit', $item->namaBarang->unit) }}" required
+                                                                name="unit" type="text" required
+                                                                class="form-control" id="unit">
+                                                        </div>
+                                                        <div class="form-floating mb-3">
+                                                            <label for="floatingInput5">Stock</label>
+                                                            <input value="{{ old('stock', $item->stock) }}" disabled
+                                                                name="stock" type="number" required
+                                                                class="form-control" id="stock">
+                                                        </div>
+                                                        <div class="input-group">
+                                                            <button class="btn btn-primary warning confirm"
+                                                                onclick="return false">Update</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- END EDIT DATA -->
+                                @endforeach --}}
+                            </tbody>
+                        </table>
+                    </div>
+                    @endif
                 </div>
             </div>
 
