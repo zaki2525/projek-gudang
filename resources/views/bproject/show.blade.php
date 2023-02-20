5156a7bda97670c3c9dc2d3ae642cfcab8465fb0
tgrsnts <tegarsantoso72@gmail.com>
2023-02-20 10:22:43 +0700

    .

--- resources/views/bproject/show.blade.php
+++ resources/views/bproject/show.blade.php
@@ -3,16 +3,16 @@
 @section('content')
     <div class="layout-px-spacing">
 
-        <div class="row layout-top-spacing">                                                      
+        <div class="row layout-top-spacing">
             <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
-                <div class="widget-content widget-content-area br-6">  
-                    <div>                
-                        <a href="{{URL::previous()}}"><button type="button" class="btn btn-primary mb-1">                        
+                <div class="widget-content widget-content-area br-6">
+                    <div>
+                        <a href="{{ URL::previous() }}"><button type="button" class="btn btn-primary mb-1">
                                 Kembali
                             </button>
                         </a>
                     </div>
-                    <h3 class="text-center">{{$bproject->nama}}</h3>   
+                    <h3 class="text-center">{{ $bproject->nama }}</h3>
                     <div class="table-responsive mb-4 mt-4">
                         <table id="html5-extension" class="table table-hover non-hover" style="width:100%">
                             <thead>
@@ -24,21 +24,30 @@
                                     <th>Unit</th>
                                     <th>Remark</th>
                                 </tr>
-                            </thead>                            
+                            </thead>
                             <tbody>
-                               @foreach($items as $item)
-                               <tr align="center">
-                                <td>{{$loop->iteration}}</td>
-                                <td>{{$item->code_project}}</td>
-                                <td>{{$item->barang->nama}}</td>
-                                <td>{{$item->stock}}</td>
-                                <td>{{$item->barang->unit}}</td>
-                                <td>{{$item->remark}}</td>
-                               </tr>
-                               @endforeach 
+                                @foreach ($items as $item)
+                                    <tr align="center">
+                                        @if ($item->stock == 0)
+                                            <td style="color:red">{{ $loop->iteration }}</td>
+                                            <td style="color:red">{{ $item->code_project }}</td>
+                                            <td style="color:red">{{ $item->barang->nama }}</td>
+                                            <td style="color:red">{{ $item->stock }}</td>
+                                            <td style="color:red">{{ $item->barang->unit }}</td>
+                                            <td style="color:red">{{ $item->remark }}</td>
+                                        @else
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $item->code_project }}</td>
+                                            <td>{{ $item->barang->nama }}</td>
+                                            <td>{{ $item->stock }}</td>
+                                            <td>{{ $item->barang->unit }}</td>
+                                            <td>{{ $item->remark }}</td>
+                                        @endif
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
-                    </div>              
+                    </div>
                 </div>
             </div>
 
